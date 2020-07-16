624b16099bad0aa5a42648301f3b1d6865d195e9
satoshigp <satoshi3104>
2020-07-16 12:08:16 +0900

    Initial commit

---
+++ Assets/Hensu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hensu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 10;
+        float fw = (float)3.14;
+        string str = "こんにちは";
+        bool bl = true;
+
+        Debug.Log(a+a);
+        Debug.Log(fw);
+        Debug.Log(str);
+        Debug.Log(bl);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
