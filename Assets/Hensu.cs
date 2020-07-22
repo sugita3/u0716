a69e2504e08679912dafade358a4762972733735
satoshigp <satoshi3104>
2020-07-22 16:14:03 +0900

    変数2

--- Assets/Hensu.cs
+++ Assets/Hensu.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hensu : MonoBehaviour
-
-
-    
+public class Hensu : MonoBehaviour  
 {
+    int vx = 20;
+    //順次処理、分岐処理、繰り返し処理
+    public void ensyu71() 
+    {
+        int vx = 5;
+        Debug.Log(vx);
+        
+        vx = 10;
+        Debug.Log(vx);
+
+
+    }
+    
+    public void ensyu72()
+    {
+        
+        Debug.Log(vx);
+    }
+
+    public void ensyu74()
+    {
+        vx += 10;
+        Debug.Log(vx);
+    }
+
     int a = 10;
     // Start is called before the first frame update
     public void Proc()
